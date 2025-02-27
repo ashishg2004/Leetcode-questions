1e62c645c0ead47a7e4e70f81a7c1e4adad4e1ca
ashishg2004 <98903125+ashishg2004@users.noreply.github.com>
2025-02-27 16:07:45 +0530

    Time: 0 ms (100.00%) | Memory: 31.5 MB (9.44%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int low=0,high=nums.size()-1;
-        while(low<=high)
-        {
-            int mid=(low+high)/2;
-            if(nums[mid]==target) 
-            return mid;
-            else if(nums[mid]>target) high=mid-1;
-            else low=mid+1;
-        }
+    int bs(vector<int>& arr,int low,int high,int target)
+    {   
+        
+        if(low>high)
         return -1;
+       int mid=(low+high)/2;
+        if(arr[mid]==target)
+        return mid;
+        else if(arr[mid]<target)
+        return bs(arr,mid+1,high,target);
+        else
+        return bs(arr,low,mid-1,target);
+    }
+    int search(vector<int>& nums, int target) {
+        return bs(nums,0,nums.size()-1,target);
     }
 };
