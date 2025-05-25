8b3d1ba579f924c349f29fe1373ae6d4fd3b43f5
ashishg2004 <98903125+ashishg2004@users.noreply.github.com>
2025-05-25 17:56:46 +0530

    Time: 0 ms (100.00%) | Memory: 15.4 MB (48.01%) - LeetSync

---
+++ 494-target-sum/target-sum.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int subsetSum(vector<int>& nums, int sum) {
+        int n = nums.size();
+        vector<vector<int>> t(n + 1, vector<int>(sum + 1, 0));  // <-- changed char to int
+
+        // Base case: There's 1 way to make sum 0 — pick none
+        for (int i = 0; i <= n; i++) {
+            t[i][0] = 1;
+        }
+
+        // Fill the table
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= sum; j++) {
+                if (nums[i - 1] <= j)
+                    t[i][j] = t[i - 1][j - nums[i - 1]] + t[i - 1][j];
+                else
+                    t[i][j] = t[i - 1][j];
+            }
+        }
+
+        return t[n][sum];
+    }
+
+    int findTargetSumWays(vector<int>& arr, int target) {
+        int sum = 0;
+        for (int i = 0; i < arr.size(); i++) {
+            sum += arr[i];
+        }
+
+        // Check for valid subset sum
+        if ((sum + target) % 2 != 0 || abs(target) > sum) return 0;
+
+        int ans = (target + sum) / 2;
+        return subsetSum(arr, ans);
+    }
+};
